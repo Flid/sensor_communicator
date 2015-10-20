b03614907f428b57a09a898067889a1adfdc3283
Anton Kirilenko <antony.kirilenko@gmail.com>
2015-10-20 22:33:08 +0100

    Better rain rating

--- sensors/weather.py
+++ sensors/weather.py
@@ -11,41 +11,43 @@ log = logging.getLogger(__name__)
 # There are lots of possible rain types, let's get value to each,
 # assuming that 1 is a usual rain
 
+LIGHT_RAIN = 0.2
+RAIN = 1.0
+HEAVY_RAIN = 2.0
+DRIZZLE = 0.7
+
+
 WEATHER_CODE_RAIN_WEIGHTS = {
-    200: 0.5,  # thunderstorm with light rain
-    201: 1.0,  # thunderstorm with rain
-    202: 1.5,  # thunderstorm with heavy rain
-    210: 0.1,  # light thunderstorm
-    211: 0.1,  # thunderstorm
-    212: 0.1,  # heavy thunderstorm
-    221: 0.1,  # ragged thunderstorm
-    230: 0.3,  # thunderstorm with light drizzle
-    231: 0.5,  # thunderstorm with drizzle
-    232: 1.0,  # thunderstorm with heavy drizzle
-    300: 0.1,  # light intensity drizzle
-    301: 0.5,  # drizzle
-    302: 1.0,  # heavy intensity drizzle
-    310: 0.5,  # light intensity drizzle rain
-    311: 1.0,  # drizzle rain
-    312: 1.5,  # heavy intensity drizzle rain
-    313: 2.0,  # shower rain and drizzle
-    314: 3.0,  # heavy shower rain and drizzle
-    321: 3.0,  # shower drizzle
-    500: 0.3,  # light rain
-    501: 1.0,  # moderate rain
-    502: 2.0,  # heavy intensity rain
-    503: 3.0,  # very heavy rain
-    504: 4.0,  # extreme rain
-    511: 5.0,  # freezing rain
-    520: 2.0,  # light intensity shower rain
-    521: 3.0,  # shower rain
-    522: 4.0,  # heavy intensity shower rain
-    531: 4.0,  # ragged shower rain
-    611: 0.1,  # sleet
-    612: 0.2,  # shower sleet
-    615: 0.5,  # light rain and snow
-    616: 1.0,  # rain and snow
-    620: 0.5,  # light shower snow
+    200: LIGHT_RAIN,            # thunderstorm with light rain
+    201: RAIN,                  # thunderstorm with rain
+    202: HEAVY_RAIN,            # thunderstorm with heavy rain
+    230: LIGHT_RAIN * DRIZZLE,  # thunderstorm with light drizzle
+    231: RAIN * DRIZZLE,        # thunderstorm with drizzle
+    232: HEAVY_RAIN * DRIZZLE,  # thunderstorm with heavy drizzle
+    300: LIGHT_RAIN * DRIZZLE,  # light intensity drizzle
+    301: RAIN * DRIZZLE,        # drizzle
+    302: HEAVY_RAIN * DRIZZLE,  # heavy intensity drizzle
+    310: LIGHT_RAIN,            # light intensity drizzle rain
+    311: RAIN,                  # drizzle rain
+    312: HEAVY_RAIN,            # heavy intensity drizzle rain
+    313: RAIN,                  # shower rain and drizzle
+    314: HEAVY_RAIN,            # heavy shower rain and drizzle
+    321: RAIN * DRIZZLE,        # shower drizzle
+    500: LIGHT_RAIN,            # light rain
+    501: RAIN,                  # moderate rain
+    502: HEAVY_RAIN,            # heavy intensity rain
+    503: HEAVY_RAIN * 2,        # very heavy rain
+    504: HEAVY_RAIN * 4,        # extreme rain
+    511: HEAVY_RAIN * 4,        # freezing rain
+    520: LIGHT_RAIN,            # light intensity shower rain
+    521: RAIN,                  # shower rain
+    522: HEAVY_RAIN,            # heavy intensity shower rain
+    531: HEAVY_RAIN,            # ragged shower rain
+    611: RAIN,                  # sleet
+    612: RAIN,                  # shower sleet
+    615: LIGHT_RAIN,            # light rain and snow
+    616: RAIN,                  # rain and snow
+    620: LIGHT_RAIN,            # light shower snow
 }
 
 class WeatherSensor(Sensor):
